25bfd2adced177ce988a8b7aef81236f934a20b2
CholponDev <januzakova.cholpon@gmail.com>
2025-12-19 16:29:01 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let body = document.getElementById("body")
+
+// body.style.display = "flex"
+body.style.justifyContent = "space-around"
+body.style.alignItems = "center"
+body.style.flex = "direction"
+body.style.height = "100vh"
+
+let block = document.getElementById("div1")
+
+block.style.width = "500px"
+block.style.height = "250px"
+block.style.backgroundColor = "blue"
+block.style.borderRadius = "50px"
+block.style.margin = "20px"
+
+let block2 = document.getElementById("div2")
+
+block2.style.width = "250px"
+block2.style.height = "250px"
+block2.style.backgroundColor = "coral"
+block2.style.borderRadius = "50px"
+block2.style.margin = "20px"
+
+
+let block3 = document.getElementById("div3")
+
+block3.style.width = "250px"
+block3.style.height = "250px"
+block3.style.backgroundColor = "red"
+block3.style.borderRadius = "50%"
+block3.style.margin = "20px"
+
+
+let block4 = document.getElementById("div4")
+
+block4.style.width = "300px"
+block4.style.height = "300px"
+block4.style.backgroundColor = "red"
+block4.style.backgroundImage = "url('img.jpeg')"
+block4.style.backgroundSize = "cover";
+block4.style.margin = "20px"
